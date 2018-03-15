8bb0ca2f469a7e3c66a03bb0c67ebff3084b9b33
Jérémy Giral <jeremy.giral@BIAL-X.local>
2018-03-15 15:39:37 +0100

    second

--- routes/index.js
+++ routes/index.js
@@ -78,15 +78,15 @@ function processV2Request (request, response) {
       // If the response to the user includes rich responses or contexts send them to Dialogflow
       var responseJson = {};
       // Define the text response
-      //responseJson.fulfillmentText = "test" || responseToUser.fulfillmentText;
-      // Optional: add rich messages for integrations (https://dialogflow.com/docs/rich-messages)
-      //if (responseToUser.fulfillmentMessages) {
+      // responseJson.fulfillmentText = "test" || responseToUser.fulfillmentText;
+      // //Optional: add rich messages for integrations (https://dialogflow.com/docs/rich-messages)
+      // if (responseToUser.fulfillmentMessages) {
       //  responseJson.fulfillmentMessages = responseToUser.fulfillmentMessages;
-      //}
-      // Optional: add contexts (https://dialogflow.com/docs/contexts)
-      if (responseToUser.outputContexts) {
-        responseJson.outputContexts = responseToUser.outputContexts;
-      }
+      // }
+      // // Optional: add contexts (https://dialogflow.com/docs/contexts)
+      // if (responseToUser.outputContexts) {
+      //   responseJson.outputContexts = responseToUser.outputContexts;
+      // }
       if(responseToUser.followup_event_input){
         responseJson.followup_event_input=responseToUser.followup_event_input;
       }
