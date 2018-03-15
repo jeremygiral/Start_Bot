90d2d765aecd07a95c765cfd426c08757770972f
Jérémy Giral <jeremy.giral@BIAL-X.local>
2018-03-15 11:44:59 +0100

    second

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.use(bodyParser.json());
 router.post("/echo", function(req, res) {
   console.log(req.body.queryResult.queryText);
   if (req.body.queryResult.queryText){
-    var rep1=req.body.queryResult.queryText
+    var rep1=req.body.queryResult.queryText;
     if (rep1.toLowerCase==="oui"){
       /*return res.json({
       speech: "Très bien, où en êtes vous dans la construction de votre Business Plan ? ( Commencement/ En Cours / En finition )",
@@ -21,91 +21,102 @@ router.post("/echo", function(req, res) {
     });*/
     console.log("ok webhook oui");
     return res.json({
-      "followupEvent": {
+      "fulfillmentText": "",
+      "fulfillmentMessages": [],
+      "outputContexts": [],
+      "followupEventInput": {
         "name": "FOui",
         "data": {
           "paramOui":rep1
+        }
+      },
+      {
+        "followupEvent": {
+          "name": "FOui",
+          "data": {
+            "paramOui":rep1
+          }
         }
       }
     });
   }
   else {
-      /*return res.json({
-        speech: "Très bien, où en êtes vous dans la construction de votre Business Plan ? ( Commencement/ En Cours / En finition )",
-        displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
-      });*/
-
-      return res.json({
-        "followupEvent": {
-          "name": "FNon",
-          "data": {
-            "paramNon":rep1
-          }
-        }
-      });
-    }
-  }
-  return res.json({
-    speech: speech,
+    /*return res.json({
+    speech: "Très bien, où en êtes vous dans la construction de votre Business Plan ? ( Commencement/ En Cours / En finition )",
     displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
-    source: "webhook-echo-sample"
+  });*/
+
+  return res.json({
+    "followupEvent": {
+      "name": "FNon",
+      "data": {
+        "paramNon":rep1
+      }
+    }
   });
+}
+}
+return res.json({
+  speech: speech,
+  displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
+  source: "webhook-echo-sample"
+});
 });
 
 module.exports = router;
 /*if (req.body.result.parameters.Reponse10){
-  return res.json({
-    speech: speech,
-    displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
+return res.json({
+speech: speech,
+displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
 
-  });
+});
 
 }
 if (req.body.result.parameters.Reponse9){
-  return res.json({
-    speech: speech,
-    displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
-  });
+return res.json({
+speech: speech,
+displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
+});
 }
 if (req.body.result.parameters.Reponse8){
-  return res.json({
-    speech: speech,
-    displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
-  });
+return res.json({
+speech: speech,
+displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
+});
 }
 if (req.body.result.parameters.Reponse7){
-  return res.json({
-    speech: speech,
-    displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
-  });
+return res.json({
+speech: speech,
+displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
+});
 }
 if (req.body.result.parameters.Reponse6){
-  return res.json({
-    speech: speech,
-    displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
-  });
+return res.json({
+speech: speech,
+displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
+});
 }
 if (req.body.result.parameters.Reponse5){
-  return res.json({
-    speech: speech,
-    displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
-  });
+return res.json({
+speech: speech,
+displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
+});
 }
 if (req.body.result.parameters.Reponse4){
-  return res.json({
-    speech: speech,
-    displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
-  });
+return res.json({
+speech: speech,
+displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
+});
 }
 if (req.body.result.parameters.Reponse3){
-  return res.json({
-    speech: speech,
-    displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
-  });
+return res.json({
+speech: speech,
+displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
+});
 }
 if (req.body.result.parameters.Reponse2){
-  return res.json({
-    speech: "Au cours de la création de votre entreprise, ou durant son existence avez-vous élaboré un Business Plan ?",
-    displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
-  });
+return res.json({
+speech: "Au cours de la création de votre entreprise, ou durant son existence avez-vous élaboré un Business Plan ?",
+displayText: speech+'\n'+testparam+'\n'+testaction+'\n'+testcontext,
+});
 }*/
