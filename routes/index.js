ded5ae0e987a978cb21c4012af74d945e755ff3e
Jérémy Giral <jeremy.giral@BIAL-X.local>
2018-03-15 13:23:11 +0100

    second

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 
 var functions = require('firebase-functions'); // Cloud Functions for Firebase library
 //var DialogflowApp = require('actions-on-google').DialogflowApp; // Google Assistant helper library
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest(function(request, response){
+router.post("/echo", function(request, response) {
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
   if (request.body.result) {
